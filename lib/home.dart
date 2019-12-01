10f68341947873827078e7e1db2a561fd2cfe99b
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-12-01 02:24:36 +0100

    Added new function to get API data

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = 'https://api.hgbrasil.com/finance?format=json&key=5987d27c';
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
 
 class Home extends StatefulWidget {
   @override
